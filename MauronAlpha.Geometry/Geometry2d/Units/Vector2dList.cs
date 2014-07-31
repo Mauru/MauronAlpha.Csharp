da535280aab83be5a4699e2bc8435a9341905660
Mauru <mauru@mauru.net>
2014-08-01 01:26:08 +0200

    more updates to dataList

--- MauronAlpha.Geometry/Geometry2d/Units/Vector2dList.cs
+++ MauronAlpha.Geometry/Geometry2d/Units/Vector2dList.cs
@@ -2,15 +2,6 @@
 
 namespace MauronAlpha.Geometry.Geometry2d.Units {
 
-	public class Vector2dList:MauronCode_dataList<Vector2d> {
-		public Vector2dList Instance {
-			get {
-				Vector2dList ret = new Vector2dList(this);
-				return ret;
-			}
-		}
-		public Vector2dList(){}
-		public Vector2dList(Vector2dList list) {}
-	}
+	public class Vector2dList:MauronCode_dataList<Vector2d> {}
 
 }
--- MauronAlpha/HandlingData/MauronCode_dataList.cs
+++ MauronAlpha/HandlingData/MauronCode_dataList.cs
@@ -1,15 +1,56 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace MauronAlpha.HandlingData {
 
 	public class MauronCode_dataList<T>:MauronCode_dataObject {
-		public List<T> Data;
-		public MauronCode_dataList(Type T) {
+		private List<T> L_data;
+		public MauronCode_dataList() {
 			
 		}
-		public T[] AsArray { get {
-			return Data.ToArray();
-		} }
+		public T[] AsArray { 
+			get {
+				return L_data.ToArray();
+			}
+		}
+		public MauronCode_dataList<T> Add(T obj){
+			L_data.Add(obj);
+			return this;
+		}
+		public MauronCode_dataList<T> Remove(T obj){
+			L_data.Remove(obj);
+			return this;
+		}
+		public bool Contains(T obj) {
+			return L_data.Contains(obj);
+		}
+		public bool ContainsKey(int i) {
+			return (L_data[i]!=null);
+		}
+
+		public delegate void Delegate_performeach(T obj);
+		public MauronCode_dataList<T> Each(Delegate_performeach doStuff){
+			foreach(T obj in L_data){
+				doStuff(obj);
+			}
+			return this;
+		}
+
+		public MauronCode_dataList<T> Clear() {
+			L_data=new List<T>();
+			return this;
+		}
+
+		//return an instance of this list, do not instance the objects
+		public MauronCode_dataList<T> Instance { get {
+			MauronCode_dataList<T> ret = new MauronCode_dataList<T>();
+			foreach(T obj in L_data) {
+				ret.Add(obj);
+			}
+			return this;
+		}}
+
+
 	}
 
 }
