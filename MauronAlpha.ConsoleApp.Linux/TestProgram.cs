4848db8d8244fb1143a5feb5c16e7a04b8af59b5
Mauru <Mauru>
2014-08-06 03:37:00 +0200

    Event System

--- MauronAlpha.ConsoleApp.Linux/TestProgram.cs
+++ MauronAlpha.ConsoleApp.Linux/TestProgram.cs
@@ -9,10 +9,7 @@ namespace MauronAlpha.ConsoleApp.Linux
 		public static void Main (string[] args)
 		{
 			//Create environment
-			MauronConsole m = new MauronConsole("Testing MauronCode_eventClock");
-			MauronCode_eventClock MasterClock = new MauronCode_eventClock ();
-			MauronCode_eventShedule UpdateTimer = new MauronCode_eventShedule (MasterClock);
-	
+			MauronConsole m = new MauronConsole("Testing MauronCode_eventClock");	
 		}
 	}
 }
