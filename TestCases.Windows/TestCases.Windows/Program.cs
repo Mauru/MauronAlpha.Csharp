2fe4e957aa9ecc88affac28fbd97778d9be629b2
mauru@mauru.net <mauru@mauru.net>
2014-12-02 09:43:16 +0100

    Testcase to find a bug in Tee Key Declarations

---
+++ TestCases.Windows/TestCases.Windows/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MauronAlpha.HandlingData;
+
+namespace TestCases.Windows {
+	class Program {
+		static void Main (string[] args) {
+			string[] KEYS_regions=new string[4] {"header","content","input","footer"};
+			MauronCode_dataTree<string,object> tests = new MauronCode_dataTree<string,object>(KEYS_regions);
+
+			System.Console.ReadKey();
+			
+		}
+	}
+}
