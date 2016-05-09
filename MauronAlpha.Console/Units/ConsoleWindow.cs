2deaf2bc9f0f3f6b142d40d7b0d33b6fdc143176
mauru <mauru@mauru.net>
2016-05-09 20:57:46 +0200

    More Console work

---
+++ MauronAlpha.Console/Units/ConsoleWindow.cs
@@ -0,0 +1,20 @@
+﻿using MauronAlpha.Geometry.Geometry2d.Units;
+using MauronAlpha.Forms.Units;
+
+using MauronAlpha.Console.Utility;
+
+namespace MauronAlpha.Console.Units {
+	
+	public class ConsoleWindow:ConsoleComponent {
+
+		public string Title;
+
+		public ConsoleRenderer Renderer;
+
+		public Vector2d Dimensions;
+
+		public FormUnit_textField Text;
+
+	}
+
+}
